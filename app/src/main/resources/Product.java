50b68f59ec008bea567dc303aa28e69341214c4f
rahuldemocenter <rahs@softwareag.com>
2023-04-13 12:04:47 +0530

    hello

---
+++ app/src/main/resources/Product.java
@@ -0,0 +1,24 @@
+public class Product {
+    private String name;
+    private String id;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getPrice() {
+        return price;
+    }
+    public void setPrice(String price) {
+        this.price = price;
+    }   
+}
