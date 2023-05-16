5bed09c145b3fd54e1828ba759fabb815b7730ee
rahuldemocenter <rahs@softwareag.com>
2023-05-16 11:53:17 +0530

    updated App.java.

--- app/src/main/java/my/java/gradle/project/App.java
+++ app/src/main/java/my/java/gradle/project/App.java
@@ -5,7 +5,7 @@ package my.java.gradle.project;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World! How are you!!!";
     }
 
     public static void main(String[] args) {
